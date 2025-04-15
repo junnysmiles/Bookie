14b4dace22bf43b925e222f728db060217957fba
Jun Gan <junyangan@hotmail.com>
2025-04-14 23:18:04 -0400

    added material UI because its easier for me to style, also added MUI icons and added a navbar

--- app/admin/create/page.js
+++ app/admin/create/page.js
@@ -1,16 +1,14 @@
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import NavBar from '../navbar.js'
 
 export const metadata = {
-    title: "Bookie - Admin"
+    title: "Bookie - Add Book"
 }
 
-export default function Admin() {
+export default function Create() {
     return (
         <>
-            <NavBar />
-            <h1 className="font-extrabold">Admin Page</h1>
+            <h1 className="font-extrabold">Add Book Page</h1>
             <Stack spacing={2} direction="row">
                 <Button variant="text">Text</Button>
                 <Button variant="contained">Contained</Button>
--- app/collection/page.js
+++ app/collection/page.js
@@ -1,6 +1,14 @@
+import NavBar from '../navbar.js'
+
+export const metadata = {
+    title: "Bookie - Collection"
+}
+
+
 export default function Collection() {
     return (
-        <>
+        <> 
+            <NavBar />
             <h1>Collection Page</h1>
         </>
     )
